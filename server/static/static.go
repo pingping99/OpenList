bf2c572ce2fd245ab51a8c42dd03ebc33516dc25
Jhon Wick <63088567+pingping99@users.noreply.github.com>
2026-09-18 22:36:26 +0800

    feat(dedup): rename menu title from '重复文件清理' to '清理'
    - rename sidemenu item to '清理'
    - add entry and manage bundles to no-cache header rule in static.go

--- server/static/static.go
+++ server/static/static.go
@@ -187,7 +187,7 @@ func Static(r *gin.RouterGroup, noRoute func(handlers ...gin.HandlerFunc)) {
 		r.Use(func(c *gin.Context) {
 			for _, folder := range folders {
 				if strings.HasPrefix(c.Request.RequestURI, fmt.Sprintf("/%s/", folder)) {
-					if strings.Contains(c.Request.RequestURI, "Dedup") {
+					if strings.Contains(c.Request.RequestURI, "Dedup") || strings.Contains(c.Request.RequestURI, "entry") || strings.Contains(c.Request.RequestURI, "manage") {
 						c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
 					} else {
 						c.Header("Cache-Control", "public, max-age=15552000")
